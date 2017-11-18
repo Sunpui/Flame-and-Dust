61e741647fe856bed9d2ceb5ab38f2542e5f59cd
Binkyface <33262142+Binkyface@users.noreply.github.com>
2017-11-18 03:33:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ module.exports = function FlameAndDust(dispatch) {
 	let cid, pid;
 	
 	dispatch.hook('S_ACTION_STAGE', 1, event => {
-		if (!enabled || event.stage != 0 || event.model !== 4000) {
+		if (!enabled || event.stage !== 0 || event.model !== 4000) {
 			return;
 		}
-		if (event.skill === 1203110998 || event.skill === 1203111013) { //Dust right front
+		if (event.skill === 1203110998 || event.skill === 1203111023) { //Dust right front
 			skillDebuff = event;
-			if (debuff === -1) {
-				return;
+			if (debuff !== -1) {
+				dustFlame(event, true);
 			}
-			dustFlame(event, debuff, true);
 		}
-		if (event.skill === 1203111018 || event.skill === 1203111012) { //Flame right front
+		if (event.skill === 1203111018 || event.skill === 1203111022) { //Flame right front
 			skillDebuff = event;
-			if (debuff === -1) {
-				return;
+			if (debuff !== -1) {
+				dustFlame(event, false);
 			}
-			dustFlame(event, debuff, false);
 		}
 	});
 
@@ -35,53 +33,51 @@ module.exports = function FlameAndDust(dispatch) {
 	dispatch.hook('S_LOGIN', 4, event => {	
 		cid = event.cid;
 		pid = event.playerId;
- 	  });
+    });
 	
 	dispatch.hook('S_ABNORMALITY_BEGIN', 2, event => {
-		if (950164 === event.id || 950165 === event.id) {
+		if (enabled && (950164 === event.id || 950165 === event.id)) {
 			if(event.target - cid === 0) {
 				debuff = event.id;
-				if (skillDebuff.skill === 1203110998 || skillDebuff.skill === 1203111013) { //Dust right front
-					dustFlame(skillDebuff, debuff, true);
+				if (skillDebuff.skill === 1203110998 || skillDebuff.skill === 1203111023) { //Dust right front
+					dustFlame(skillDebuff, true);
 					return;
 				}
-				if (skillDebuff.skill === 1203111018 || skillDebuff.skill === 1203111012) { //Flame right front
-					dustFlame(skillDebuff, debuff, false);
+				if (skillDebuff.skill === 1203111018 || skillDebuff.skill === 1203111022) { //Flame right front
+					dustFlame(skillDebuff, false);
 					return;
 				}
-				console.log("Something went wrong");
 			}
 		}
 	});	
 	
-	function dustFlame(skill, debuffID, dustRight) {
+	function dustFlame(skill, dustRight) {
 		if ((dustRight && debuff === 950164) || (!dustRight && debuff === 950165)) { //dust front right + player has dust; flame front right + player has flame;
 			var angle = Math.PI * (-40.0 + skill.w / 8192.0 * 45) / 180;	//skill.w goes from 0 for north, to 8192 for north-east, 16384 for east, -32768 for south, -16384 for west, -8192 for north-west
-			var beaconX = skill.x + Math.cos(angle) * 425.205841; //spawn beacon left front
-			var beaconY = skill.y + Math.sin(angle) * 425.205841;
+			var beaconX = skill.x + Math.cos(angle) * 745.0; //spawn beacon left front
+			var beaconY = skill.y + Math.sin(angle) * 745.0;
 			spawnBeacon(beaconX, beaconY, skill.z, pid);
 			angle = Math.PI * (132.3 + skill.w / 8192.0 * 45) / 180;
-			beaconX = skill.x + Math.cos(angle) * 297.32135; //spawn beacon right back
-			beaconY = skill.y + Math.sin(angle) * 297.32135;
+			beaconX = skill.x + Math.cos(angle) * 617.0; //spawn beacon right back
+			beaconY = skill.y + Math.sin(angle) * 617.0;
 			spawnBeacon(beaconX, beaconY, skill.z, pid+1);
-			debuff = -1;
 		}
 		if ((!dustRight && debuff === 950164) || (dustRight && debuff === 950165)) { //dust front left + player has dust; flame front left + player has flame;
 			var angle = Math.PI * (30.0 + skill.w / 8192.0 * 45) / 180;
-			var beaconX = skill.x + Math.cos(angle) * 484.148743; //spawn beacon right front
-			var beaconY = skill.y + Math.sin(angle) * 484.148743;
+			var beaconX = skill.x + Math.cos(angle) * 804.0; //spawn beacon right front
+			var beaconY = skill.y + Math.sin(angle) * 804.0;
 			spawnBeacon(beaconX, beaconY, skill.z, pid);
 			angle = Math.PI * (-150.3 + skill.w / 8192.0 * 45) / 180;
-			beaconX = skill.x + Math.cos(angle) * 403.112885; //spawn beacon left back
-			beaconY = skill.y + Math.sin(angle) * 403.112885;
+			beaconX = skill.x + Math.cos(angle) * 723.0; //spawn beacon left back
+			beaconY = skill.y + Math.sin(angle) * 723.0;
 			spawnBeacon(beaconX, beaconY, skill.z, pid+1);
-			debuff = -1;
 		}
+		debuff = -1;
+		skillDebuff = {};
 	}
 	
 	function spawnBeacon(beaconX, beaconY, beaconZ, id) {
-		dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id: id})
-		setTimeout(() => { dispatch.toClient('S_SPAWN_DROPITEM', 2, {
+		dispatch.toClient('S_SPAWN_DROPITEM', 2, {
 			id: id,
 			x: beaconX,
 			y: beaconY,
@@ -90,7 +86,7 @@ module.exports = function FlameAndDust(dispatch) {
 			amount: 1,
 			expiry: 1,
 			owners: [{id: pid}]
-		})	}, 50)
-		setTimeout(() => { dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id: id}) }, 5000)	
+		})
+		setTimeout(() => { dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id: id}) }, 7000)	
 	}
 }
